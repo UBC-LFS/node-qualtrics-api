998d622c45c8a283c8cf2aad7d5277aa16538adf
KevinCHWu <kevinwu0703@gmail.com>
2018-06-25 13:56:57 -0700

    Finished createResponseExport

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const { listSurveys, getSurvey, listMailingLists, getSurveyId, listDistribution, getDistribution } = require('./src/index')
+const { listSurveys, getSurvey, listMailingLists, getSurveyId, listDistribution, getDistribution, getResponseExport } = require('./src/index')
 
-getDistribution('Test#2', 'EMD_3fOdA3v2QGcQJh3').then(x => console.log(x))
+getResponseExport('Test#2').then(x => console.log(x))
---
+++ source/getResponseExport.js
@@ -0,0 +1,17 @@
+import createResponseExport from './internal/createResponseExport'
+import getSurveyId from './getSurveyId'
+
+require('dotenv').config()
+
+const qualtricsDomain = process.env.QUALTRICS_API_DOMAIN
+
+/**
+ * Retrieves information for one survey
+ * @param {String} survey the name of the Survey
+ * @return {Promise} A promise that resolves to a Survey object: https://api.qualtrics.com/docs/get-survey
+*/
+
+export default async function getResponseExport (survey) {
+  const id = await (getSurveyId(survey))
+  return createResponseExport(qualtricsDomain + 'responseexports', id)
+}
