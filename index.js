3b3a4d9887a3cf74f4aa7301c4af5b67c153832a
KevinCHWu <kevinwu0703@gmail.com>
2018-06-25 11:27:05 -0700

    finished getDistribution, and added comments for param

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const { listSurveys, getSurvey, listMailingLists, getSurveyId, listDistribution } = require('./src/index')
+const { listSurveys, getSurvey, listMailingLists, getSurveyId, listDistribution, getDistribution } = require('./src/index')
 
-listDistribution('Test#2').then(x => console.log(x))
+getDistribution('Test#2', 'EMD_3fOdA3v2QGcQJh3').then(x => console.log(x))
