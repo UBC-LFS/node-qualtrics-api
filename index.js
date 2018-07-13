bd46c418a7de6bb95a1eb45b4a0dd51e357d6890
ellehcim8174 <michelle.mak.8174@gmail.com>
2018-07-13 15:59:04 -0700

    working on polling - Timeout issue

--- index.js
+++ index.js
@@ -14,17 +14,28 @@ const fs = require('fs')
 const { promisify } = require('util')
 const fetch = require('./src/internal/fetch')
 
-getSurveyIds('LFS LC Loans 2018')
+const responseID = getSurveyIds('test')
   .then(ids => ids[0])
   .then(id => getResponseExportId(id))
-  .then(({ id }) => getResponseExportProgress(id))
-  .then(progress => console.log(progress))
-//   .then(x => console.log(x))
-  // .then(url => fetch(url))
-  // .then(stream => promisify(fs.writeFile)('./output.zip', stream))
 
-// fetch('https://ubc.qualtrics.com/API/v3/responseexports/ES_86kaau3pgf5fn4uvsbd05r8nlg/file')
-//   .then(x => console.log(x))
+function responseProgress (id) {
+  return id.then(({ id }) => getResponseExportProgress(id))
+}
+// const responseProgress = responseID
+//   .then(({ id }) => getResponseExportProgress(id))
+
+const getProgress = setInterval(function () { responseProgress(responseID) }, 50)
+
+if (responseProgress.percentComplete === 100) {
+  clearInterval(getProgress)
+} else console.log(getProgress)
+
+// //   .then(x => console.log(x))
+//   // .then(url => fetch(url))
+//   // .then(stream => promisify(fs.writeFile)('./output.zip', stream))
+
+// // fetch('https://ubc.qualtrics.com/API/v3/responseexports/ES_86kaau3pgf5fn4uvsbd05r8nlg/file')
+// //   .then(x => console.log(x))
 
 fetch('https://ubc.qualtrics.com/API/v3/responseexports/ES_44qdqdlsedldjkulgmlhev8rdq/file')
   // .then(stream => console.log(stream))
