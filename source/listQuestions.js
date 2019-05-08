7d8d980e770275f36cf21ac2b0ba63ffc69e180d
austinkwok1997 <austin.kwok@protonmail.com>
2019-05-08 16:06:22 -0700

    added listQuestions

---
+++ source/listQuestions.js
@@ -0,0 +1,17 @@
+import fetchAll from "./internal/fetchAll";
+import getSurveyIds from "./getSurveyIds";
+
+require("dotenv").config();
+
+const qualtricsDomain = process.env.QUALTRICS_API_DOMAIN;
+
+/**
+ * Retrieves all questions in Survey
+ * @param {String} name name of Survey
+ * @return {Promise} A promise that resolves to a list of Survey questions: https://api.qualtrics.com/reference#list-questions-1
+ */
+
+export default async function listQuestions(name) {
+  const id = await getSurveyIds(name);
+  return fetchAll(qualtricsDomain + "survey-definitions/" + id + "/questions");
+}
