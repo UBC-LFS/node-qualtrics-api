18499590492944eb4c46115ad6a00171c820f158
austinkwok1997 <austin.kwok@protonmail.com>
2019-05-21 13:59:05 -0700

    survey quotas already done. just updated deleteSurvey documentation instead

--- source/deleteSurvey.js
+++ source/deleteSurvey.js
@@ -8,7 +8,7 @@ const qualtricsDomain = process.env.QUALTRICS_API_DOMAIN
 /**
  * Deletes Survey
  * @param {String} survey the name of the Survey
- * @return {Promise} A promise that resolves to deleting a survey: https://api.qualtrics.com/docs/get-survey
+ * @return {Promise} A promise that resolves to deleting a survey: https://api.qualtrics.com/reference#delete-survey
 */
 
 export default async function deleteSurvey(survey) {
