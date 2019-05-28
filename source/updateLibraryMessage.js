4b5288de37013a53ae424aff17af210f868ecafb
austinkwok1997 <austin.kwok@protonmail.com>
2019-05-28 14:46:06 -0700

    Finished updateLibraryMessage and also removed import statement from createLibraryMessage

---
+++ source/updateLibraryMessage.js
@@ -0,0 +1,22 @@
+import update from './internal/update'
+
+require('dotenv').config()
+
+const qualtricsDomain = process.env.QUALTRICS_API_DOMAIN
+
+/**
+ * Updates a Library Message
+ * @param {String} library the libraryId
+ * @param {String} messageId id of message
+ * @param {String} newDescription the updated description
+ * @param {String} newMessage the updated message
+ * @return {Promise} A promise that resolves to a Response update: https://api.qualtrics.com/reference#update-response
+*/
+
+export default function updateLibraryMessage(library, messageId, newDescription, newMessage) {
+    const updateMessage = {
+        'messages': { 'en': newMessage },
+        'description': newDescription
+    }
+    return update(`${qualtricsDomain}libraries/${library}/messages/${messageId}`, updateMessage);
+}
