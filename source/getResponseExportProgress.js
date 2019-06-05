fdb14dedcf90cc3a601b543f10bd05d1fa9b8184
austinkwok1997 <austin.kwok@protonmail.com>
2019-06-05 16:25:58 -0700

    updated getResponseExportProgress

--- source/getResponseExportProgress.js
+++ source/getResponseExportProgress.js
@@ -1,10 +1,18 @@
 import fetch from './internal/fetch'
+import getSurveyIds from './getSurveyIds'
 
 require('dotenv').config()
 
 const qualtricsDomain = process.env.QUALTRICS_API_DOMAIN
 
-export default async function getResponseExportProgress (exportId) {
-  const response = await fetch(qualtricsDomain + 'responseexports/' + exportId)
-  return response.result
+/**
+ * Retries a Progress report on a Response Export
+ * @param {String} survey the name of the Survey
+ * @param {String} exportProgressId Export progress ID returned by the export creation call
+ * @return {Promise} A promise that resolves to a Response Export progress report: https://api.qualtrics.com/reference#get-response-export-progress-1
+*/
+
+export default async function getResponseExportProgress(survey, exportId) {
+  const id = await (getSurveyIds(survey));
+  return fetch(`${qualtricsDomain}surveys/${id}/export-responses/${exportId}`)
 }
